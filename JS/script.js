84102dacb66dc79f16136bd59717eb8528e72844
Erik Sales <eriksales1@hotmail.com>
2023-01-27 17:28:04 -0500

    css updates

--- JS/script.js
+++ JS/script.js
@@ -5,7 +5,7 @@ bagsJson.map((item, i) =>{
 
     document.querySelector('.bag-area').append(bagItem);
 
-    bagItem.querySelector('.item-img img').src = item.img;
+    bagItem.querySelector('.item-img img').src = item.img
     bagItem.querySelector('.bag-item--price').innerHTML = `$ ${item.price}`;
     bagItem.querySelector('.bag-item--name').innerHTML = item.name;
     bagItem.querySelector('.bag-item--notes').innerHTML = item.notes;
