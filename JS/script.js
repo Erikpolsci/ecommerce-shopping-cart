2fdd74053523816d47877573aa1b7b31d9340ab5
Erik Sales <eriksales1@hotmail.com>
2023-01-29 12:14:10 -0500

    new improvements and functions

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 bagsJson.map((item, i) =>{
     
     let bagItem = document.querySelector('.bags .bag-item').cloneNode(true)
-    //console.log(bagItem)
+    console.log(bagItem)
 
     document.querySelector('.bag-area').append(bagItem);
 
@@ -9,6 +9,34 @@ bagsJson.map((item, i) =>{
     bagItem.querySelector('.bag-item--price').innerHTML = `$ ${item.price}`;
     bagItem.querySelector('.bag-item--name').innerHTML = item.name;
     bagItem.querySelector('.bag-item--notes').innerHTML = item.notes;
+
+    bagItem.querySelector('.bag-item a').addEventListener('click',(e) =>{
+        e.preventDefault()
+        console.log('clicou')
+
+        document.querySelector('.modalWindow').style.display = 'flex'
+        document.querySelector('.imgBag img').src = item.img;
+        document.querySelector('.bagInfo h1').innerHTML = item.name;
+        document.querySelector('.bagInfo--notes').innerHTML = item.notes;
+        document.querySelector('.bagInfo--actualPrice').innerHTML = `$ ${item.price}`;
+
+
+    })
+
+    document.querySelector('.bagInfo--cancelButton').addEventListener('click', () => {
+        document.querySelector('.modalWindow').style.display = 'none'
+
+
+    })
+
+    
+
+
+
+    
+
+
+
 
 
 
