ca25c4a256641d7bfe675a3568b5532f0c5a14c9
Erik Sales <eriksales1@hotmail.com>
2023-01-30 10:41:32 -0500

    refactored functions

--- JS/script.js
+++ JS/script.js
@@ -1,44 +1,79 @@
-bagsJson.map((item, i) =>{
-    
-    let bagItem = document.querySelector('.bags .bag-item').cloneNode(true)
-    console.log(bagItem)
+const select = (element) => document.querySelector(element);
+const selectAll = (element) => document.querySelectorAll(element);
 
-    document.querySelector('.bag-area').append(bagItem);
+const openModalWindow = () => {
+    select('.modalWindow').style.opacity = 0;
+    select('.modalWindow').style.display = 'flex';
+    setTimeout(() => {
+        select('.modalWindow').style.opacity = 1
+    }, 150)
+}
 
+const closeModalWindow = () => {
+    select('.modalWindow').style.opacity = 0
+    setTimeout(() => {
+        select('.modalWindow').style.display = 'none'
+    }, 500)
+}
+
+const btnClose = () => {
+
+    select('.bagInfo--cancelButton').addEventListener('click', closeModalWindow)
+}
+
+const fillBagItemsInfo = (bagItem, item) => {
     bagItem.querySelector('.item-img img').src = item.img
     bagItem.querySelector('.bag-item--price').innerHTML = `$ ${item.price}`;
     bagItem.querySelector('.bag-item--name').innerHTML = item.name;
     bagItem.querySelector('.bag-item--notes').innerHTML = item.notes;
+}
 
-    bagItem.querySelector('.bag-item a').addEventListener('click',(e) =>{
+const fillModalInfo = (item) => {
+
+
+    select('.imgBag img').src = item.img;
+    select('.bagInfo h1').innerHTML = item.name;
+    select('.bagInfo--notes').innerHTML = item.notes;
+    select('.bagInfo--actualPrice').innerHTML = `$ ${item.price}`;
+}
+
+
+
+bagsJson.map((item, i) => {
+
+    let bagItem = document.querySelector('.bags .bag-item').cloneNode(true)
+    console.log(bagItem)
+
+
+    select('.bag-area').append(bagItem);
+
+    fillBagItemsInfo(bagItem, item);
+
+    bagItem.querySelector('.bag-item a').addEventListener('click', (e) => {
         e.preventDefault()
-        console.log('clicou')
+        //console.log('clicou')
+        openModalWindow();
+        //console.log(openModalWindow)
+        fillModalInfo(item);
 
-        document.querySelector('.modalWindow').style.display = 'flex'
-        document.querySelector('.imgBag img').src = item.img;
-        document.querySelector('.bagInfo h1').innerHTML = item.name;
-        document.querySelector('.bagInfo--notes').innerHTML = item.notes;
-        document.querySelector('.bagInfo--actualPrice').innerHTML = `$ ${item.price}`;
+
+
 
 
     })
-
-    document.querySelector('.bagInfo--cancelButton').addEventListener('click', () => {
-        document.querySelector('.modalWindow').style.display = 'none'
-
-
-    })
-
-    
-
-
-
-    
+    btnClose();
 
 
 
 
 
 
-    
+
+
+
+
+
+
+
+
 })
