0705e284212e7df8adc8c3e0662790d565bd144f
Erik Sales <eriksales1@hotmail.com>
2023-02-01 08:15:48 -0500

    new setup

--- Shopping-cart/assets/JS/coffeeBags.js
+++ Shopping-cart/assets/JS/coffeeBags.js
@@ -2,42 +2,42 @@ let bagsJson = [
     {
       id: 1,
       name: "Kaua Coffee",
-      img: "./assets/images/IMG_7423.JPG",
+      img: "./Shopping-cart/assets/images/IMG_7423.JPG",
       price: 25.00,
       notes: "Vanilla, molasses, floral"
     },
     {
       id: 2,
       name: "Bernardes M1 Coffee",
-      img: "./assets/images/IMG_7424.JPG",
+      img: "./Shopping-cart/assets/images/IMG_7424.JPG",
       price: 22.00,
       notes: "Guarana, orange and molasses"
     },
     {
       id: 3,
       name: "Marcio Heleno Coffee",
-      img: "./assets/images/IMG_7423.JPG",
+      img: "./Shopping-cart/assets/images/IMG_7423.JPG",
       price: 28.00,
       notes: "Strawberry, Blackcurrant, Molasses, Full-bodied"
     },
     {
       id: 4,
       name: "Erita Benincá Coffee",
-      img: "./assets/images/IMG_7424.JPG",
+      img: "./Shopping-cart/assets/images/IMG_7424.JPG",
       price: 20.00,
       notes: "Very sweet, honey, apricot, medium body and delicate acidity"
     },
     {
       id: 5,
       name: "Guima Coffee",
-      img: "./assets/images/IMG_7423.JPG",
+      img: "/Shopping-cart/assets/images/IMG_7423.JPG",
       price: 20.00,
       notes: "Grape Bubble Gum, Raspberry,  Lavender"
     },
     {
       id: 6,
       name: "Gaia Coffee",
-      img: "./assets/images/IMG_7424.JPG",
+      img: "./Shopping-cart/assets/images/IMG_7424.JPG",
       price: 35.00,
       notes: "Papaya, Rapadura, Lemon"
     },
